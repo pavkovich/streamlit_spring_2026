e254101e2451d7ded2ab6af5e054213f673dce30
Robert Pavkovich <rpavkovich@gmail.com>
2026-03-13 11:41:03 -0400

    Implement checkbox to show/hide raw data
    Added a checkbox to toggle the display of raw data.

--- uber_nyc_pickups.py
+++ uber_nyc_pickups.py
@@ -25,8 +25,12 @@ data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text("Done! (using st.cache_data)")
 
-st.subheader('Raw data')
-st.write(data)
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+# st.subheader('Raw data')
+# st.write(data)
 
 st.subheader('Number of pickups by hour')
 hist_values = np.histogram(
@@ -35,6 +39,7 @@ hist_values = np.histogram(
 st.bar_chart(hist_values)
 
 hour_to_filter = st.slider('hour', 0, 23, 17)
+# min: 0h, max: 23h, default: 17h
 
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
