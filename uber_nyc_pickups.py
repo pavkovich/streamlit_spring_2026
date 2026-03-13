0e8284da68d5b7e7d5aae93338ca0a649b5115a7
Robert Pavkovich <rpavkovich@gmail.com>
2026-03-13 11:56:18 -0400

    Enable histogram and bar chart for pickups by hour
    Uncomment code for histogram and bar chart display.

--- uber_nyc_pickups.py
+++ uber_nyc_pickups.py
@@ -32,11 +32,16 @@ if st.checkbox('Show raw data'):
 # st.subheader('Raw data')
 # st.write(data)
 
+# st.subheader('Number of pickups by hour')
+# hist_values = np.histogram(
+    # data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+
 st.subheader('Number of pickups by hour')
 hist_values = np.histogram(
     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-
 st.bar_chart(hist_values)
+
+# st.bar_chart(hist_values)
 
 hour_to_filter = st.slider('hour', 0, 23, 17)
 # min: 0h, max: 23h, default: 17h
